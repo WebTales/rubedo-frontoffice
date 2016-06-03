2debb368d164e3e5efd622eec2580abe1a3ee2ab
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-06-03 15:54:22 +0200

    localise ga events

--- src/modules/rubedo/rubedo.js
+++ src/modules/rubedo/rubedo.js
@@ -235,8 +235,8 @@
         return serviceInstance;
     }]);
 
-    app.controller("RubedoController",['RubedoBlockTemplateResolver','RubedoImageUrlService','RubedoAuthService','RubedoFieldTemplateResolver','snapRemote','RubedoPageComponents','RubedoTranslationsService','$scope','RubedoClickStreamService','$rootScope','UXUserService','UXPageService','UXSessionService',
-        function(RubedoBlockTemplateResolver,RubedoImageUrlService,RubedoAuthService,RubedoFieldTemplateResolver,snapRemote, RubedoPageComponents, RubedoTranslationsService,$scope,RubedoClickStreamService,$rootScope,UXUserService,UXPageService,UXSessionService){
+    app.controller("RubedoController",['RubedoBlockTemplateResolver','RubedoImageUrlService','RubedoAuthService','RubedoFieldTemplateResolver','snapRemote','RubedoPageComponents','RubedoTranslationsService','$scope','RubedoClickStreamService','$rootScope','UXUserService','UXPageService','UXSessionService','$route',
+        function(RubedoBlockTemplateResolver,RubedoImageUrlService,RubedoAuthService,RubedoFieldTemplateResolver,snapRemote, RubedoPageComponents, RubedoTranslationsService,$scope,RubedoClickStreamService,$rootScope,UXUserService,UXPageService,UXSessionService,$route){
         var me=this;
         //break nav on non-page routes
         $scope.$on("$locationChangeStart",function(event, newLoc,currentLoc){
@@ -380,7 +380,7 @@
         };
         me.sendGaEvent = function(cat, label) {
             if(window.ga) {
-                window.ga('send', 'pageview', cat+label);
+                window.ga('send', 'pageview', '/'+$route.current.params.lang+cat+label);
             }
         };
 
