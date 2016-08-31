60f133ab2b75848fc49da83b822815565708c286
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-08-31 11:09:56 +0200

    WIP map

--- src/modules/rubedoBlocks/controllers/InsightsMapController.js
+++ src/modules/rubedoBlocks/controllers/InsightsMapController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller("InsightsMapController",["$scope",'RubedoClickStreamService', function($scope, RubedoClickStreamService){
+angular.module("rubedoBlocks").lazy.controller("InsightsMapController",["$scope",'RubedoClickStreamService','$element', function($scope, RubedoClickStreamService,$element){
     var me = this;
     var config=$scope.blockConfig;
     me.activeFacets={};
@@ -8,6 +8,15 @@ angular.module("rubedoBlocks").lazy.controller("InsightsMapController",["$scope"
     };
     me.filterParam={
     };
+    me.map={
+        center:{
+            latitude:48.8567,
+            longitude:2.3508
+        },
+        zoom:config.zoom ? config.zoom : 14
+    };
+    me.height = config.height ? config.height + "px" : "500px";
+    me.geocoder = new google.maps.Geocoder();
     if (config.events&&config.events.length>0){
         me.filterParam.event=config.events;
     }
@@ -15,8 +24,25 @@ angular.module("rubedoBlocks").lazy.controller("InsightsMapController",["$scope"
         startDate:moment().subtract(config.interval,"days").format("YYYY-MM-DD"),
         endDate:moment().format("YYYY-MM-DD")
     };
+    me.mapControl={ };
+    //map events
+    me.mapTimer = null;
+    me.mapEvents = {
+        "bounds_changed": function (map) {
+            clearTimeout(me.mapTimer);
+            me.mapTimer = setTimeout(function() {
+                me.drawGraph();
+            }, 300);
+        }
+    };
     me.drawGraph=function(){
-        me.queryParams.filters=JSON.stringify(me.filterParam);
+        var params=angular.copy(me.filterParam);
+        var bounds=me.mapControl.getGMap().getBounds();
+        params.inflat=bounds.getSouthWest().lat();
+        params.suplat=bounds.getNorthEast().lat();
+        params.inflon=bounds.getSouthWest().lng();
+        params.suplon=bounds.getNorthEast().lng();
+        me.queryParams.filters=JSON.stringify(params);
         RubedoClickStreamService.getGeoAgg(me.queryParams).then(function(response){
             if (response.data.success){
                 console.log(response.data.data);
@@ -39,7 +65,14 @@ angular.module("rubedoBlocks").lazy.controller("InsightsMapController",["$scope"
         delete(me.filterParam[key]);
         me.drawGraph();
     };
+    me.display = function () {
 
-    me.drawGraph();
+        return true;
+    };
+    if (config.height&&config.height!=500){
+        setTimeout(function(){
+            $element.find(".angular-google-map-container").height(config.height);
+        },190);
+    }
 
 }]);
